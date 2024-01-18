76682ab11846e6fb42cae786523fed3f73f16977
Alexander <electromanati@gmail.com>
2024-01-18 11:13:04 +0100

    grid works now, but still need to make it checkered

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const boxArr = [];
 let wrapper = document.querySelector(".Checkered_Wrapper");
 
 //create 100 divs
-for (let i = 0; i <= 99; i++) {
-  const createElements = document.createElement("div");
-  const divAttribute = createElements.classList.add("box");
-  boxArr.push(createElements);
+for (let i = 0; i <= 999; i++) {
+  const createBoxes = document.createElement("div");
+  const addBox = createBoxes.classList.add("box");
+  boxArr.push(createBoxes);
 }
 
 //append the 100 divs to wrapper
@@ -22,3 +22,11 @@ boxArr.forEach((box) => {
 });
 
 //work on making a checkered board that has almost the same functionality
+
+//brug setinterval og setTimeout til at lave noget sjovt med det her hvor
+boxArr.forEach((box, index) => {
+  let modulusOperator = 8;
+  if (index % modulusOperator === 0) {
+    box.style.backgroundColor = "red";
+  }
+});
